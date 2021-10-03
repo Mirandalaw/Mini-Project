a5401276a527267eef96bd07bc7dd15b20f8cb08
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-10-03 21:42:52 +0900

    Update script.js

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,17 +1,33 @@
-const newYears = '1 Jan 2021'
-
-function countdown(){
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
-
-    const totalseconds = (newYearsDate-currentDate)/1000;
-
-    const days = Math.floor(seconds / 3600 / 24);
-    const hours = Math.floor(seconds/3600)%24;
-    const minutes = Math.floor(seconds/60)%60;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Project from pjh</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <h1>Today to do list</h1>
     
-    console.log(days,hours,minutes);
-}
-countdown();
-
-setInterval(countdown,1000);
+    <div class="countdown-container">
+        <div class="countdown-el days-c">
+            <p class ="big-text" id="days">0</p>
+            <span>days</span>
+        </div>
+        <div class="countdown-el hours-c">
+            <p class ="big-text" id="hours">0</p>
+            <span>hours</span>
+        </div>
+        <div class="countdown-el mins-c">
+            <p class ="big-text" id="mins">0</p>
+            <span>mins</span>
+        </div>
+        <div class="countdown-el sec-c">
+            <p class ="big-text" id="secs">0</p>
+            <span>secs</span>
+        </div>
+    </div>
+</body>
+</html>
