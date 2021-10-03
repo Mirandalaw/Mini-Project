f05d29c4a18e975f2b69de219eabd662f691ab04
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2021-10-03 21:19:03 +0900

    Update script.js

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,1 +1,17 @@
-document.getElementById('currentDate').value = new Date().toISOString().substring(0,10);;
+const newYears = '1 Jan 2021'
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalseconds = (newYearsDate-currentDate)/1000;
+
+    const days = Math.floor(seconds / 3600 / 24);
+    const hours = Math.floor(seconds/3600)%24;
+    const minutes = Math.floor(seconds/60)%60;
+    
+    console.log(days,hours,minutes);
+}
+countdown();
+
+setInterval(countdown,1000);
